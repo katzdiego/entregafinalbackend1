7166f5e20c97f3334b723b486a2ed5a400869bc1
Diego Katz <katzdiego7@gmail.com>
2025-05-16 22:43:53 -0300

    carts

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,31 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
+const Cart = require("../models/Cart");
 
-// Vista principal: listado de productos
 router.get("/", async (req, res) => {
   try {
     const products = await Product.find().lean();
     res.render("index", { products });
   } catch (error) {
-    console.error("❌ Error al cargar la vista:", error);
+    console.error("Error al cargar la vista:", error);
     res.status(500).send("Error al cargar los productos");
   }
 });
 
-// Vista de detalle de producto
 router.get("/product/:pid", async (req, res) => {
   try {
     const product = await Product.findById(req.params.pid).lean();
     if (!product) return res.status(404).send("Producto no encontrado");
 
-    // ID real del carrito creado con Postman
     const cartId = "6827e62f3f97bfe07c12a17d";
-
     res.render("productDetail", { product, cartId });
   } catch (error) {
-    console.error("❌ Error al cargar el detalle:", error);
+    console.error("Error al cargar el detalle:", error);
     res.status(500).send("Error al cargar el producto");
+  }
+});
+
+router.get("/carts/:cid", async (req, res) => {
+  try {
+    const cart = await Cart.findById(req.params.cid).populate("products.product").lean();
+    if (!cart) return res.status(404).send("Carrito no encontrado");
+
+    res.render("cartDetail", { cart });
+  } catch (error) {
+    console.error("Error al cargar el carrito:", error);
+    res.status(500).send("Error al cargar el carrito");
   }
 });
 
